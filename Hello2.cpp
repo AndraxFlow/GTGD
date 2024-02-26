d463e01a4b3d1c5bea69b7a40c74bd89d1461233
andrax <andronov_maksimka@list.ru>
2024-02-26 21:57:10 +0300

    first1

---
+++ Hello2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+void hello(){
+    std::cout << "abcd" << std::endl;
+}
+
+
---
+++ hello_buddy.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main(){
+    std::cout << "abcd" << std::endl;
+    return 0;
+}
