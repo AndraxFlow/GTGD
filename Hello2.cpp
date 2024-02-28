35985018cb0543d6186069685a70f3bbfce7cb67
andrax <andronov_maksimka@list.ru>
2024-02-28 18:56:01 +0300

    1rst modify

--- Hello2.cpp
+++ Hello2.cpp
@@ -2,6 +2,10 @@
 
 void hello(){
     std::cout << "abcd" << std::endl;
+
+    int z = 5;
+    int a = 8;
+    std::cout << z + a << std::endl;
 }
 
 
--- hello_buddy.cpp
+++ hello_buddy.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-
+#include"Hello2.cpp"
 int main(){
     std::cout << "abcd" << std::endl;
+    return 0;
+    hello();
     return 0;
 }
